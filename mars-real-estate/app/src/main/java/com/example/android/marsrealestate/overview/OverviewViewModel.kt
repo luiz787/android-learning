d6f0b110bbcbb365fb674e003b71517f40192caa
Luiz Berto <luiz.berto@dtidigital.com.br>
2021-04-27 09:03:20 -0300

    wip: detail screen

--- mars-real-estate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ mars-real-estate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -40,6 +40,10 @@ class OverviewViewModel : ViewModel() {
     val properties: LiveData<List<MarsProperty>>
         get() = _properties
 
+    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
+    val naviagteToSelectedProperty: LiveData<MarsProperty>
+        get() = _navigateToSelectedProperty
+
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
@@ -76,5 +80,13 @@ class OverviewViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
+    }
+
+    fun displayPropertyDetails(marsProperty: MarsProperty) {
+        _navigateToSelectedProperty.value = marsProperty
+    }
+
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
     }
 }
