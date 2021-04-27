68f7fc7a630e92428056f3067818ddde122c71c9
Luiz Berto <diasbertoluiz@gmail.com>
2021-04-27 09:39:11 -0300

    finish mars-real-estate app

--- mars-real-estate/app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ mars-real-estate/app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -18,7 +18,7 @@ package com.example.android.marsrealestate.detail
 
 import android.app.Application
 import androidx.lifecycle.*
-import com.example.android.marsrealestate.detail.DetailFragment
+import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.network.MarsProperty
 
 /**
@@ -38,5 +38,15 @@ class DetailViewModel(marsProperty: MarsProperty, app: Application) : AndroidVie
             true -> R.string.display_price_monthly_rental
             false -> R.string.display_price
         }, it.price)
+    }
+
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type,
+            app.applicationContext.getString(
+                    when (it.isRental) {
+                        true -> R.string.type_rent
+                        false -> R.string.type_sale
+                    }
+            ))
     }
 }
