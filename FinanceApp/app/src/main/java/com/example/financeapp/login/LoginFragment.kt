23f6f8697ecb568e8d95e4df036ca36286127991
Luiz Berto <diasbertoluiz@gmail.com>
2021-05-04 12:07:43 -0300

    finish login

--- FinanceApp/app/src/main/java/com/example/financeapp/login/LoginFragment.kt
+++ FinanceApp/app/src/main/java/com/example/financeapp/login/LoginFragment.kt
@@ -1,9 +1,14 @@
 package com.example.financeapp.login
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -32,7 +37,29 @@ class LoginFragment : Fragment() {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
         }
 
+        binding.loginButton.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            viewModel.handleLogin(email, password)
+
+            view?.let { view -> hideKeyboardFrom(requireContext(), view) }
+        }
+
+        viewModel.loginResult.observe(viewLifecycleOwner, {
+            if (it == "Login successful") {
+                Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.i("LoginFragment", it)
+            }
+        })
+
         return binding.root
+    }
+
+    private fun hideKeyboardFrom(context: Context, view: View) {
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 }
--- FinanceApp/app/src/main/java/com/example/financeapp/login/LoginViewModel.kt
+++ FinanceApp/app/src/main/java/com/example/financeapp/login/LoginViewModel.kt
@@ -1,6 +1,40 @@
 package com.example.financeapp.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+
+    val email = MutableLiveData("")
+    val password = MutableLiveData("")
+
+    private val _loginResult = MutableLiveData("")
+    val loginResult: LiveData<String>
+        get() = _loginResult
+
+    fun handleLogin(email: String, password: String) {
+        if (!validateLoginFields(email, password)) {
+            return
+        }
+        doLogin()
+    }
+
+    private fun validateLoginFields(email: String, password: String): Boolean {
+        if (email.isBlank()) {
+            _loginResult.value = "Invalid email"
+            return false
+        }
+        if (password.isBlank()) {
+            _loginResult.value = "Invalid password"
+            return false
+        }
+        return true
+    }
+
+    private fun doLogin() {
+        Thread.sleep(500)
+
+        _loginResult.value = "Login successful"
+    }
 }
